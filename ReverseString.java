0d9a2a033e27e4d5dd4b04eb391e05cc8b7de8dc
saiveerendranath <40311376+saiveerendranath@users.noreply.github.com>
2020-01-04 14:43:06 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -2,6 +2,7 @@ import java.util.*;
 class ReverseString{
    public static void main(String[] args) {
       String string = "abcdef";
+      String s2="meow";
       String reverse = new StringBuffer(string).reverse().toString();
       System.out.println("\nString before reverse: "+string);
       System.out.println("String after reverse: "+reverse);
